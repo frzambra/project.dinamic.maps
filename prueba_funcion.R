e416085f9e1570abe8eedb3c4e4f458b781e5141
frzambra <frzambra@gmail.com>
2014-08-08 23:13:07 -0400

    probado prueba_funcion.R con todas las regiones y para algunas fechas

--- prueba_funcion.R
+++ prueba_funcion.R
@@ -7,20 +7,27 @@ require('rgeos')
 source('modisDate.R')
 source('mapCom.R')
 
-#data.frame en que cada columna corresponde a un periodo y cada fila a una comuna de la regi?n
-data<-read.csv2('data/r4_data.csv')
-
-#objeto Date que corresponde a las fechas de cada columna
+#objeto Date que corresponde a las fechas en cada columna
 dates<-modisDate(as.Date(c('2000-02-18','2014-07-28')))
 
+#region para la cual se desea hacer el mapa entre "r4" (cuarta region)
+#a "r10" (decima region)
+reg<-"r4"
+fecha<-1 #corresponde a dates[1]
+
+#data.frame en que cada columna corresponde a un periodo y cada fila a una comuna de la regi?n
+data<-read.csv2(paste0('data/',reg,'_data.csv'))
+
 #poligono de la regi?n del B?o-B?o con las 54 comunas
-shp<-readShapePoly('shp/r4.shp')
+shp<-readShapePoly(paste0('shp/',reg,'.shp'))
 
 #aplica funci?n mapCom para generar mapa con valores continuos
-mapCom(shp,data,fill='V1',shp.u='COD_COMUNA',data.u='cod')
+#names(data)[3] para que plotee la tercera columna del data.frame
+mapCom(shp,data,fill="V1",shp.u='COD_COMUNA',data.u='cod')
 
 #Defino clases de valores para mapear de forma discreta
-data2map<-data.frame(cod=data$cod,value=cut(data$V1*100,seq(0,100,10),include.lowest=TRUE))
+#Busca la ubicacion 
+data2map<-data.frame(cod=data$cod,value=cut(data[,2+fecha]*100,seq(0,100,10),include.lowest=TRUE))
 
 #Define colores para las clases
 values<-c("[0,10]"="#8B0000","(10,20]"="#A43800","(20,30]"="#BE7100","(30,40]"="#D8AA00",
