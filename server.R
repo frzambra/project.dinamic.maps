c88760ba826053c939c8d91dad7ad4bc610a2095
frzambra <frzambra@gmail.com>
2014-08-15 12:21:14 -0400

    cambio paleta de colores y cambios magnitud valores indice en data

--- server.R
+++ server.R
@@ -14,9 +14,18 @@ shinyServer(function(input, output) {
     
     shp <- readShapePoly(file.path('shp', paste0("r", input$region_input, ".shp")))
     col <- paste0("V", which.min(abs(ymd(dates_select) - ymd(input$date_input)))[1])
-    p <- mapCom(shp, data, fill=col, shp.u='COD_COMUNA', data.u='cod')
+    
+    data2map<-data.frame(cod=data$cod,value=cut(data[,col],seq(0,100,10),include.lowest=TRUE))
+    
+    #Define colores para las clases
+    values<-c("[0,10]"="#8B0000","(10,20]"="#A43800","(20,30]"="#BE7100","(30,40]"="#D8AA00",
+              "(40,50]"="#F2E200","(50,60]"="#E2ED00","(60,70]"="#AACB00","(70,80]"="#71A800",
+              "(80,90]"="#388600","(90,100]"="#006400")
+    
+    p<-mapCom(shp,data=data2map,fill='value',shp.u='COD_COMUNA',data.u='cod',fill.values=values)
+    #p <- mapCom(shp, data, fill=col, shp.u='COD_COMUNA', data.u='cod')
     print(p)
-  
+    
   })
   
   output$table_sequia <- renderDataTable({
